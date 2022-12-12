c90cfacb3bab23f37e78194714236d7d4b7d1aa2
Gregory Rogachev <kotsmilewtm@gmail.com>
2022-12-13 00:26:43 +0300

    v0.0.15-a

--- src/adapter/stores/events.ts
+++ src/adapter/stores/events.ts
@@ -3,7 +3,7 @@ import type { modules } from 'evm-ext'
 
 type EventsState = modules.events.EventsState['events']
 
-export const useEvents = defineStore<'$events', EventsState>('$events', {
+export const useEvents = defineStore('$events', {
   state: () => ({
     listeners: [],
     listenerId: 1,
